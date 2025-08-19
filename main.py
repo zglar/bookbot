255d27bfc3090bf163f35cf189b2fa2fb6fa6195
zglar <stephanus.agung96@gmail.com>
2025-08-19 16:18:02 +0700

    made report

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from stats import get_num_words
 from stats import get_num_characters
+from stats import char_count_report
 
 def main():
-    char_count = get_num_characters()
-    print(char_count)
+    filepath = "books/frankenstein.txt"
+    words_count = get_num_words()
+    char_count = char_count_report()
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words_count} total words")
+    print("--------- Character Count -------")
+    for ch, cnt in char_count.items():
+        print(f"{ch}: {cnt}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -13,5 +13,11 @@ def get_num_characters():
     book_text = get_book_text(filepath).lower().split()
     for word in book_text:
         for char in word:
-            char_counts[char] = char_counts.get(char,0) + 1
+            if char.isalpha():
+                char_counts[char] = char_counts.get(char,0) + 1
     return char_counts
+
+def char_count_report():
+    char_count_list = get_num_characters()
+    sorted_char = dict(sorted(char_count_list.items(), key=lambda item: item[1], reverse=True))
+    return sorted_char
